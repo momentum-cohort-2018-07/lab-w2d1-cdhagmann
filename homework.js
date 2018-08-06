a3475902f0c35356d345de0964799bde932f601f
Christopher Dean Hagmann <cdhagmann@gmail.com>
2018-08-06 12:26:36 -0400

    Finished first 9 problems.

--- homework.js
+++ homework.js
@@ -12,19 +12,39 @@ var leftover = candies % people;
 // 3. Create a function called greeting that returns "Hello, <name>!",
 // where <name> is the name given as an argument to the function.
 
+function greeting (name) {
+    return "Hello, " + name + "!";
+}
+
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not.
 
+function isOdd (number) {
+    return Math.abs(number) % 2 === 1;
+}
+
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not.
+
+function isEven (number) {
+    return Math.abs(number) % 2 === 0;
+}
 
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius.
 
+function fahrenheitToCelsius (temp) {
+    return 5 * (temp - 32) / 9.0;
+}
+
 // 6. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
 // temperature in Fahrenheit.
+
+function celsiusToFahrenheit(temp) {
+    return 9/5.0 * temp + 32;
+}
 
 // 7. Create a function called fahrenheitToKelvin that takes a
 // Fahrenheit temperature as an argument and returns the
@@ -33,9 +53,21 @@ var leftover = candies % people;
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
 
+function fahrenheitToKelvin(temp) {
+    return 5 * (temp - 32) / 9.0 + 273.15;
+}
+
 // 8. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
+
+function lesser (a, b) {
+    if (a <= b) {
+        return a;
+    } else {
+        return b;
+    }
+}
 
 // 9. Create a function called multigreeting that takes a name
 // and a language code and returns a version of "Hello, <name>!"
@@ -49,6 +81,19 @@ var leftover = candies % people;
 //
 // If any other language code is used, return nothing.
 
+function multigreeting (name, languageCode){
+    if (languageCode.toLowerCase() === "en") {
+        return "Hello, " + name + "!";
+    } else if (languageCode.toLowerCase() === "es") {
+        return "¡Hola, " + name + "!";
+    } else if (languageCode.toLowerCase() === "fr") {
+        return "Bonjour, " + name + "!";
+    } else if (languageCode.toLowerCase() === "eo") {
+        return "Saluton, " + name + "!";
+    } else {
+        return ;
+    }
+}
 // 10. Create a function, howOld, that takes a birthdate and a another
 // date (the "current date") and returns how many years old the person
 // with that birthdate was on that date. The dates should be JavaScript
